a40e1b0d8d8af81ba6991e68b3708eceb6c7bafd
Gaurav Singh <gauravnumber@gmail.com>
2021-12-26 20:34:08 +0530

    login message done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Button, Form, Container } from "semantic-ui-react";
+import { Message, Button, Form, Container } from "semantic-ui-react";
 import { useMutation } from "@apollo/client";
 
 import { userStoreAction } from "../reducer/userReducer";
@@ -9,12 +9,21 @@ import { LOGIN } from "../queries";
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [state, setState] = useState({
+    message: "",
+    messageStatus: "",
+  });
 
   const dispatch = useDispatch();
 
   const [login, { loading, error, data }] = useMutation(LOGIN, {
     onError: (err) => {
-      console.log(err.graphQLErrors[0].message);
+      // console.log(err.graphQLErrors[0].message);
+      setState({
+        ...state,
+        message: err.graphQLErrors[0].message,
+        messageStatus: "red",
+      });
     },
   });
 
@@ -26,8 +35,13 @@ const Login = () => {
     if (data !== undefined) {
       dispatch(userStoreAction(data.login));
       localStorage.setItem("jwtToken", data.login.token);
-      console.log(`JSON.stringify(data.login)`, JSON.stringify(data.login));
+      // console.log(`JSON.stringify(data.login)`, JSON.stringify(data.login));
       localStorage.setItem("loginUser", JSON.stringify(data.login));
+      setState({
+        ...state,
+        message: "Account successfully login.",
+        messageStatus: "green",
+      });
 
       // console.log(`data.login`, data.login);
     }
@@ -47,8 +61,13 @@ const Login = () => {
 
     // setUsername("");
     // setPassword("");
+    setState({
+      ...state,
+      message: "",
+      messageStatus: "",
+    });
 
-    console.log(`data`, data);
+    // console.log(`data`, data);
     // console.log(`username`, username);
     // console.log(`password`, password);
   };
@@ -68,6 +87,9 @@ const Login = () => {
         />
         <button className="ui button">login</button>
         {/* <Button>submit</Button> */}
+        {state.message && (
+          <Message content={state.message} color={state.messageStatus} />
+        )}
       </Form>
       {/* <form onSubmit={handleLogin}>
         <input type="text" onChange={(e) => setUsername(e.target.value)} />
