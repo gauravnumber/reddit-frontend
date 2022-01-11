9b8314b0089a9e0920e3993354a83a55c48b333f
Gaurav Singh <gauravnumber@gmail.com>
2022-01-11 12:10:30 +0530

    adding type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly BACKEND_URI: string;
+  readonly HI: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
