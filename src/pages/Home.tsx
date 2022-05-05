5531891ad3d4705f9add5fcdd074204dbeadceac
Gaurav Singh <gauravnumber@gmail.com>
2022-05-05 21:27:05 +0530

    loading added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { useQuery, useLazyQuery } from "@apollo/client";
-
-import Post from "@components/Post";
-
 import { GET_RECENT_POSTS } from "@/queries";
-import { notificationState, RootState, sortState } from "@/types";
+import { RootState, sortState } from "@/types";
+import { useLazyQuery } from "@apollo/client";
+import Post from "@components/Post";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+// import {Dimmer, Loader } from "semantic-ui-react";
 
 const Home = () => {
   const sort = useSelector<RootState, sortState>((state) => state.sort);
@@ -23,19 +21,22 @@ const Home = () => {
   useEffect(() => {
     getRecentPostsBySorting();
   }, [sort]);
-  // console.log(`result.data`, result.data);
 
-  // console.log(`notification`, notification);
+  // console.log(`result.loading`, result.loading);
+
+  if (result.loading) return "loading...";
+  // if (result.loading) return <Loader />;
 
   return (
     <div>
       <h1>Welcome to clone of reddit.</h1>
-      {/* <p></p> */}
       {/* <ul>
         <li>
           <Link to="/r/funny">r/funny</Link>
         </li>
       </ul> */}
+      {/* {result.loading && "loading..."} */}
+      {/* {result.loading && <Loader>loading</Loader>} */}
       {result.data && <Post posts={result.data.getRecentPosts} />}
     </div>
   );
