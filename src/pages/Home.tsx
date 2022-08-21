193c392028516754c6eec502c53c8bf394beed20
Gaurav Singh <gauravnumber@gmail.com>
2022-08-21 13:42:19 +0530

    image worked on getRecentPosts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { postType, RootState, sortState } from "@/types";
 import { useLazyQuery } from "@apollo/client";
 import {
@@ -13,27 +15,27 @@ import {
   Typography,
 } from "@mui/material";
 import { ArrowDownward, ArrowUpward } from "@mui/icons-material";
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
 // import { Dimmer, Loader } from "semantic-ui-react";
 import { GET_RECENT_POSTS } from "@/queries";
 
 const Home = () => {
-  const sort = useSelector<RootState, sortState>((state) => state.sort);
+  // const sort = useSelector<RootState, sortState>((state) => state.sort);
   // const notification = useSelector<RootState, notificationState>(
   //   (state) => state.notification
   // );
   // const result = useQuery(GET_RECENT_POSTS, {
   const [getRecentPostsBySorting, result] = useLazyQuery(GET_RECENT_POSTS, {
     variables: {
-      sort: sort,
+      sort: "new",
     },
   });
 
   useEffect(() => {
     getRecentPostsBySorting();
     // console.log(`sort`, sort);
-  }, [sort]);
+  }, []);
+  // }, [sort]);
 
   // console.log(`result.loading`, result.loading);
   if (result.loading)
@@ -50,7 +52,10 @@ const Home = () => {
   //     </Dimmer>
   //   );
 
-  console.log(`result.data`, result.data);
+  // console.log(`result.data`, result.data);
+  // console.log(
+  //   `data:${result?.data?.getRecentPosts[3]?.image?.contentType};base64,${result?.data?.getRecentPosts[3]?.image?.data}`
+  // );
 
   return (
     <Box sx={{ mt: 2 }}>
@@ -58,7 +63,11 @@ const Home = () => {
         result.data.getRecentPosts.map((post: postType) => (
           <Card key={post._id} sx={{ mb: 3 }}>
             <CardHeader
-              title={`r/${post.subreddit.name}`}
+              title={
+                <Link to={`/r/${post.subreddit.name}`}>
+                  r/{post.subreddit.name}
+                </Link>
+              }
               // &bull;
               subheader={`u/${post.owner.username} \u2022 4years`}
             />
@@ -78,8 +87,10 @@ const Home = () => {
                 component="img"
                 // height="200"
                 // image={`https://source.unsplash.com/500x300?sig=${post._id}`}
-                image={`data:${post.image.contentType};base64,${post.image.data}`}
-                alt="random"
+                // image={`data:${post.image.contentType};base64,${post.image.data}`}
+                // image={`http://localhost:4000/uploads/${post.image.data}`}
+                image={`${import.meta.env.VITE_UPLOAD}/${post.image.data}`}
+                alt={post.title}
               />
             )}
             <CardActions disableSpacing>
